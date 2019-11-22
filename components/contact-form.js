3c017c77e0c7c799f1e047b92281b0b04381ad69
Håkon Ellingsen <hakonweb@gmail.com>
2019-11-22 13:25:57 +0100

    :bug: Fix contact form

--- components/contact-form.js
+++ components/contact-form.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './button';
+import fetch from 'cross-fetch';
 import classNames from './contact-form.module.css';
+
+function encode(data) {
+	return Object.keys(data)
+		.map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+		.join('&');
+}
 
 function Input({ label, ...props }) {
 	return (
@@ -21,6 +28,33 @@ function Textarea({ label, ...props }) {
 }
 
 function ContactForm() {
+	const [state, setState] = useState({
+		description: '',
+		email: '',
+		name: '',
+	});
+
+	function handleChange(e) {
+		setState({ ...state, [e.target.name]: e.target.value });
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const form = e.target;
+
+		fetch('/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: encode({
+				'form-name': form.getAttribute('name'),
+				...state,
+			}),
+		})
+			.then(() => alert("Takk for din henvendelse"))
+			.catch(error => alert(error));
+	}
+
 	return (
 		<>
 			<p>
@@ -32,27 +66,40 @@ function ContactForm() {
 				.
 			</p>
 
-			<form name="contact" method="POST" data-netlify="true">
+			<form
+				name="contact"
+				method="POST"
+				data-netlify="true"
+				data-netlify-honeypot="bot-field"
+				onSubmit={handleSubmit}
+			>
+				<input type="hidden" name="form-name" value="contact" />
 				<Input
 					label="Ditt navn"
 					type="text"
 					name="name"
+					value={state.name}
 					placeholder="Kari-Ola Nordmann..."
 					required
+					onChange={handleChange}
 				/>
 				<Input
 					label="Din epost"
 					type="email"
 					name="email"
+					value={state.email}
 					placeholder="epost@eksempel.no"
 					required
+					onChange={handleChange}
 				/>
 				<Textarea
 					label="Hva trenger du hjelp med?"
 					rows="4"
 					name="description"
+					value={state.description}
 					placeholder="Jeg trenger hjelp med..."
 					required
+					onChange={handleChange}
 				/>
 				<Button type="submit">Send inn</Button>
 			</form>
