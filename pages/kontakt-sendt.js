ecfaa6a4fd87d8b71b52a22754b01cd04761a6f4
Håkon Ellingsen <hakonweb@gmail.com>
2019-11-22 12:48:49 +0100

    :speech_balloon:

--- pages/kontakt-sendt.js
+++ pages/kontakt-sendt.js
@@ -4,9 +4,9 @@ import Layout from '../components/layout';
 function Index() {
 	return (
 		<Layout>
-			<h1>Skjemaet ble innsendt</h1>
+			<h1>Takk for henvendelsen</h1>
 
-			<p>Vi tar kontakt straks vi ser din henvendelse.</p>
+			<p>Vi tar kontakt så fort det lar seg gjøre.</p>
 		</Layout>
 	);
 }
